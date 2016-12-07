28e3dcf92e6f11dc3dded0d9ec0327044e0bb9c1
Aamnah <hello@aamnah.com>
2016-12-07 08:38:12 +0500

    + react-router

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
 import App from './components/App'
 
 require('./styles.scss')
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path='/' component={App} />
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
+  </Provider>,
   document.querySelector('#root')
 )
